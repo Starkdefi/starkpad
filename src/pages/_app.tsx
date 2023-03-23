18f8672de8f0ea51ad19dfb5734466a8b00cc936
Iddriss Raaj <idd.iraaj@gmail.com>
2023-03-23 12:22:24 +0000

    setup: update website head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,38 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Starkpad | By StarkDefi</title>
+        <meta
+          name="description"
+          content="Starkpad by StarkDefi: Your one-stop platform for launching and investing in StarkNet projects. Discover the next blue-chip opportunities and kickstart your journey with us."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        ></link>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        ></link>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        ></link>
+        <link rel="manifest" href="/site.webmanifest"></link>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
