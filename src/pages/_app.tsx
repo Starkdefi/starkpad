3fa92bd07c36df3b8d18913af4be9c25c00f652a
Iddriss Raaj <idd.iraaj@gmail.com>
2023-03-28 21:23:29 +0000

    chore: remove old placeholder

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function StarkPad({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
