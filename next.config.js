577b40f717291ad736c13086865b01450acaa8e2
Iddriss Raaj <idd.iraaj@gmail.com>
2023-03-28 21:25:08 +0000

    chore: configs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  swcMinify: true,
+  webpack(config) {
+    config.output.publicPath = "";
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    config.module.rules.push({
+      test: /\.(js|jsx|ts|tsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: "babel-loader",
+        options: {
+          presets: ["next/babel", "@babel/preset-env", "@babel/preset-react"],
+        },
+      },
+    });
 
-module.exports = nextConfig
+    return config;
+  },
+};
+module.exports = nextConfig;
