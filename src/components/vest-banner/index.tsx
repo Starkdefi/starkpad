77f8a20644960cc36e7a5dc75953b9cfeed9c42c
Iddriss Raaj <idd.iraaj@gmail.com>
2023-04-03 23:33:29 +0000

    update vesting mobile

--- src/components/vest-banner/index.tsx
+++ src/components/vest-banner/index.tsx
@@ -16,28 +16,30 @@ export default function VestingBanner() {
         style={{ zIndex: "-1", objectFit: "cover", objectPosition: "center" }}
       />
       <div className={styles.content}>
-        <div className="xl:w-[60%] lg:w-[60%] md:w-[47%] flex flex-col gap-5 z-[1]">
+        <div className="xl:w-[60%] lg:w-[60%] md:w-[47%] w-3/5 flex flex-col md:gap-5 gap-2 z-[1]">
           <p
             className={classNames(
               "font-CeraPro-Bold font-bold text-white",
-              "xl:text-[44px] lg:text-[36px] md:text-[24px] text-3xl",
-              "xl:leading-[55px] lg:leading-[45px] md:leading-[36px] leading-[45px]"
+              "xl:text-[44px] lg:text-[36px] md:text-[24px] text-sm",
+              "xl:leading-[55px] lg:leading-[45px] md:leading-[36px] leading-[140%]"
             )}
           >
             Vest your way to the next generation of StarkNet startups
           </p>
-          <p className="text-green_light font-CeraPro-Medium font-medium xl:text-2xl xl:leading-10 lg:text-xl md:text-base text-xl lg:leading-normal leading-9">
-            Vesting $SDC grants you access to all <br className="hidden md:block" />
+          <p
+            className={classNames(
+              "text-green_light font-CeraPro-Medium font-medium",
+              "xl:text-2xl xl:leading-10 lg:text-xl md:text-base",
+              " text-xs lg:leading-normal leading-[140%]"
+            )}
+          >
+            Vesting $SDC grants you access to all{" "}
+            <br className="hidden md:block" />
             StarkPad launches
           </p>
         </div>
         <div className={styles.starkLogo}>
-          <Image
-            alt="Stark3D Logo"
-            placeholder="blur"
-            src={Stark3D}
-            className="md:h-auto h-52 md:z-auto z-[-1] object-contain md:object-center object-right-top"
-          />
+          <Image alt="Stark3D Logo" placeholder="blur" src={Stark3D} />
         </div>
       </div>
     </div>
