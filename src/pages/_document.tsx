98e477ffbf80535ce4cdb1578efea167afbf8828
Iddriss Raaj <idd.iraaj@gmail.com>
2023-04-04 21:54:52 +0000

    add analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,18 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+          <Script
+          src="//www.googletagmanager.com/gtag/js?id=G-BQHT2MXTRM"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-BQHT2MXTRM');
+        `}
+        </Script>
       </body>
     </Html>
   )
